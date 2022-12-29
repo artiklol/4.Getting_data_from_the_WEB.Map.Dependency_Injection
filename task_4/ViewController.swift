6f564570df24fe2c54fb3f9b71635ad98a283d08
Artem Sulzhenko <artiklol2012@gmail.com>
2022-12-29 16:49:34 +0300

    ADDED project, SwiftLint and podfile

---
+++ task_4/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  task_4
+//
+//  Created by Artem Sulzhenko on 29.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
